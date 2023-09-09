b2e80d688344d71a2cf4e34e0e111a0942e404f7
yoto.yotov <yoto.yotov@fadata.eu>
2023-09-09 15:33:42 +0300

    Enable anyone to mint FusionToken

--- fusion-contract/contracts/FusionToken.sol
+++ fusion-contract/contracts/FusionToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract FusionToken is ERC20, Ownable {
     constructor() ERC20("Fusion Token", "FSN") {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
